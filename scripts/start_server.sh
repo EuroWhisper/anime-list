c7eae4a6be6aac75003e7cd930cfdac11e19cd0c
Laurence Juden <ralphjuden@gmail.com>
2022-01-08 12:51:16 +0100

    Continuous deployment setup (#2)

---
+++ scripts/start_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# navigate into current working directory
+
+cd /home/ec2-user/anime-list
+
+# install node modules
+
+nvm use 16.13.1
+
+npm install
+
+npm install pm2 -g
+
+
+# start our node app in the background using pm2
+
+sudo pm2 start ‘npm run start’
---
+++ scripts/stop_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Stop react server
+
+echo "Killing react server"
+
+sudo pm2 kill
