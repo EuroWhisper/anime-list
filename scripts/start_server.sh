dfb5c9836f5db0e45f14d4a816905a6763d6661f
Laurence Juden <ralphjuden@gmail.com>
2022-02-26 23:45:58 +0100

    Update start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,10 +4,11 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
 . ~/.nvm/nvm.sh
 
-nvm install node 16.13.1
+nvm install 16.13.1
+
+nvm use 16.13.1
 
 sudo chmod -R 777 /home/ec2-user/anime-list
-
 
 # navigate into current working directory
 
@@ -15,7 +16,6 @@ cd /home/ec2-user/anime-list
 
 # install node modules
 
-nvm use 16.13.1
 
 npm install
 
@@ -24,6 +24,7 @@ npm install -g serve
 
 npm install pm2 -g
 
-# start our node app in the background using pm2
 
+npm run build
+# start our node app in the background using pm2
 pm2 start 'npm run serve'
